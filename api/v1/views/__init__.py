05e0b3f8fe8ec47156d50bc3fa640917cf00c21a
Abubakr Nasir <abubakr.ezalden.nasir@gmail.com>
2024-04-02 14:44:18 +0000

    citites

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,4 +5,5 @@ from flask import Blueprint
 app_views = Blueprint("base", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
+from api.v1.views.cities import *
 from api.v1.views.states import *
