da7e97529b91c63990a2443f2a7688f381501ba7
Abubakr Nasir <abubakr.ezalden.nasir@gmail.com>
2024-04-02 14:59:30 +0000

    test

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ Blueprint for API """
 from flask import Blueprint
+
+app_views = Blueprint("base", __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
 from api.v1.views.cities import *
 from api.v1.views.states import *
 
-app_views = Blueprint("base", __name__, url_prefix="/api/v1")
